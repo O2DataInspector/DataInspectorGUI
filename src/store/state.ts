f69a6d4fc89f201088346338471a00560cc0aa1c
Jakub Adam Czajka <jakub.adam.czajka@cern.ch>
2021-02-04 08:28:29 +0100

    Define Redux state

---
+++ src/store/state.ts
@@ -0,0 +1,41 @@
+type Url = string
+
+enum DisplayMethod { Default, Custom }
+
+interface Message {
+  id: string
+  sender: string
+  origin: string
+  description: string
+  subSpecification: string
+  firstTForbit: number
+  tfCounter: number
+  runNumber: number
+  startTime: number | undefined
+  duration: number | undefined
+  creationTime: number | undefined
+  taskHash: number | undefined
+  payloadSize: number
+  payloadParts: number
+  payloadSerialization: string
+  payloadSplitIndex: number
+  payloadBytes: string
+  payload: string | undefined
+  isDisplayed: boolean
+  payloadDisplay: DisplayMethod
+}
+
+interface Device {
+  isSelected: boolean
+  name: string
+  messages: Message[]
+}
+
+interface State {
+  analysisHost: Url | undefined
+  devices: Device[]
+}
+
+export default State
+export { DisplayMethod }
+export type { Device, Message, Url }
