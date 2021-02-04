23bf55b77b5158beef3bba8bb41a21ee6411bdc9
Jakub Adam Czajka <jakub.adam.czajka@cern.ch>
2021-02-04 08:30:11 +0100

    Define Redux selectors

---
+++ src/store/selectors.ts
@@ -0,0 +1,7 @@
+import State from './state'
+
+const selectAddress = (state: State) => state.analysisHost
+const selectSelectedDevices = (state: State) =>
+  state.devices.filter(device => device.isSelected)
+
+export { selectAddress, selectSelectedDevices }
