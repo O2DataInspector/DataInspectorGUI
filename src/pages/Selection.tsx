acd647fe0d07d4db2b7af2b36a54ffc57414d8a0
Jakub Adam Czajka <jakub.adam.czajka@cern.ch>
2021-02-04 08:43:31 +0100

    React component for the selection for inspection page

---
+++ src/pages/Selection.tsx
@@ -0,0 +1,16 @@
+import DevicesSelection from 'components/DevicesSelection'
+import NavigationBar, * as Buttons from 'components/NavigationBar'
+
+import 'components/common.css'
+import 'pages/selection.css'
+
+const Selection = () => (
+  <div id='selection' className='row2'>
+    <NavigationBar>
+      <Buttons.Disconnect />
+    </NavigationBar>
+    <DevicesSelection />
+  </div>
+)
+
+export default Selection
