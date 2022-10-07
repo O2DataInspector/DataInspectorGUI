fa6119e829372507c8013950ab10eefb34b6651f
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-10-07 16:10:33 +0200

    remove loader

--- src/pages/Connection.tsx
+++ src/pages/Connection.tsx
@@ -1,6 +1,5 @@
 import Axios from "axios";
 import React from "react";
-import { TailSpin } from "react-loader-spinner";
 import * as Redux from "react-redux";
 import * as Router from "react-router-dom";
 
@@ -11,22 +10,18 @@ import { setTopologyDetails } from "store/actions";
 import { Container, Box } from "@mui/material";
 
 const Connection = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
   const store = Redux.useStore();
   const history = Router.useHistory();
 
   function onSubmit(address: string) {
-    setIsLoading(true);
     Axios.get(address + "/available-devices")
       .then((response) => {
         const devices = response.data.split("\n");
         store.dispatch(setTopologyDetails(address, devices));
-        setIsLoading(false);
         history.push("/dashboard");
       })
       .catch((_) => {
-        alert("Failed to connect. Is the analysis task running?");
-        setIsLoading(false);
+        alert("Failed to connect. Is proxy running?");
       });
   }
 
@@ -36,17 +31,10 @@ const Connection = () => {
       <Container sx={{ height: "75%", mt: "5%" }}>
         <Box height="100%" width="50%" sx={{ mx: "auto" }}>
           <ConnectionForm onSubmit={onSubmit} />
-          {isLoading ? <Spinner /> : <br />}
         </Box>
       </Container>
     </React.Fragment>
   );
 };
-
-const Spinner = () => (
-  <div id="loader">
-    <TailSpin color="#000000A0" height="40px" />
-  </div>
-);
 
 export default Connection;
